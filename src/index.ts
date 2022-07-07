4e62e9e0f072fc4b54b9c44b313ea2567a53eadc
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-07-07 18:54:26 +0300

    added step I set {string} = {string}

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { Then } from '@cucumber/cucumber';
+import { Then, When } from '@cucumber/cucumber';
+import memory from '@qavajs/memory';
 
 /**
  * Verify that value from memory satisfies validation against other value
+ * @param {any} value1 - first value
+ * @param {Function} validation - validation
+ * @param {any} value2 - second value
  * @example I expect '$value' equals to '$anotherValue'
  * @example I expect '$value' does not contain '56'
  */
@@ -11,6 +15,9 @@ Then('I expect {text} {validation} {text}', async function (value1: any, validat
 
 /**
  * Verify that every element in array satisfies validation against other value
+ * @param {any} arr - arr
+ * @param {Function} validation - validation
+ * @param {any} value2 - expected value
  * @example I expect every element in '$arr' array to be above '$expectedValue'
  * @example I expect every element in '$arr' array to be above '50'
  */
@@ -22,3 +29,13 @@ Then(
        }
     }
 );
+
+/**
+ * Set memory value
+ * @param {string} key
+ * @param {string} value
+ * @example I set 'key' = 'value'
+ */
+When('I set {string} = {string}', function (key, value) {
+    memory.setValue(key, value);
+});
