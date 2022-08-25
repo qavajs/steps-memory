e55fa0f489410d15d8a963dc3e7fb9f0f25a926d
Alexander Galichenko <alexandr.galichenko@gmail.com>
2022-08-25 12:19:22 +0300

    updated validation lib to 0.0.2 (#6)
    Co-authored-by: Oleksandr_Halichenko <oleksandr_halichenko@epam.com>

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import { defineParameterType } from '@cucumber/cucumber';
 
 defineParameterType({
     name: 'memoryValidation',
-    regexp: /((?:is |do |does |to )?(not |to not )?(?:to )?(?:be )?(equal|have member|match|contain|above|below|greater than|less than|have type)(?:s|es)?)/,
+    regexp: /((?:is |do |does |to )?(not |to not )?(?:to )?(?:be )?(equal|strictly equal|deeply equal|have member|match|contain|above|below|greater than|less than|have type)(?:s|es)?)/,
     transformer: p => p,
     useForSnippets: false
 });
