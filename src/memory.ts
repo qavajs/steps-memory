8faa28a393856b3f859ab3a346ce6bfea2731a05
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-05-29 19:51:13 +0300

    added validation step with parameter in form of data table array (#16)

---
+++ src/memory.ts
@@ -0,0 +1,85 @@
+import { DataTable, Then, When } from '@cucumber/cucumber';
+import memory from '@qavajs/memory';
+import { getValidation } from '@qavajs/validation';
+import { getValue } from './transformers';
+import { dataTable2Object } from './utils';
+
+/**
+ * Save result of math expression and save result to memory
+ * @param expression - string expression
+ * @param key - key to store value
+ * @example When I save result of math expression '{$variable} + 42' as 'result'
+ * @example When I save result of math expression '{$random()} * 100' as 'result'
+ */
+When(
+    'I save result of math expression {string} as {string}',
+    async function (expression: string, key: string) {
+        const resolvedExpression: string = await getValue(expression);
+        const exprFn = new Function('return ' + resolvedExpression);
+        memory.setValue(key, exprFn());
+    }
+);
+
+/**
+ * Save value to memory
+ * @param {string} alias - value to save or alias for previously saved value
+ * @param {string} key - key to store value
+ * @example I save 'value' to memory as 'key'
+ * @example I save '$getRandomUser()' to memory as 'user'
+ */
+When(
+   'I save {string} to memory as {string}',
+    async function (alias: string, key: string) {
+      const value: string = await memory.getValue(alias);
+      memory.setValue(key, value);
+    }
+);
+
+/**
+ * Save value to memory
+ * @param {string} key - key to store value
+ * @param {string} value - value to save or alias for previously saved value
+ * @example I set 'key' = 'value'
+ */
+When(
+    'I set {string} = {string}',
+    async function (key: string, value: string) {
+        const resolvedValue: string = await memory.getValue(value);
+        memory.setValue(key, resolvedValue);
+    }
+);
+
+/**
+ * Save json value to memory
+ * @param {string} key - key to store value
+ * @param {string} json - multiline string
+ * @example I save json to memory as 'key':
+ * """
+ * {
+ *     "someKey": "someValue"
+ * }
+ * """
+ */
+When(
+    'I save json to memory as {string}:',
+    async function (key: string, json: string) {
+        const value: string = await memory.getValue(json);
+        memory.setValue(key, JSON.parse(value));
+    }
+);
+
+/**
+ * Save key-value pairs to memory
+ * @param {string} key - key to store value
+ * @param {string} kv - key-value
+ * @example I save key-value pairs to memory as 'key':
+ * | someKey      | 42               |
+ * | someOtherKey | $valueFromMemory |
+ */
+When(
+    'I save key-value pairs to memory as {string}:',
+    async function (key: string, kv: DataTable) {
+        const value = await dataTable2Object(kv);
+        memory.setValue(key, value);
+    }
+);
--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import { defineParameterType } from '@cucumber/cucumber';
 
 defineParameterType({
     name: 'memoryValidation',
-    regexp: /((?:is |do |does |to )?(not |to not )?(?:to )?(?:be )?(equal|strictly equal|deeply equal|have member|match|contain|above|below|greater than|less than|have type|have property|match schema)(?:s|es)?)/,
+    regexp: /((?:is |do |does |to )?(not |to not )?(?:to )?(?:be )?(equal|strictly equal|deeply equal|have member|match|contain|above|below|greater than|less than|have type|have property|match schema|include members)(?:s|es)?)/,
     transformer: p => p,
     useForSnippets: false
 });
