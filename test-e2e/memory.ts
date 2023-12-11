6098ab0b49492cdf2827498fbfd1640de33f2dcf
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-12-11 19:27:17 +0200

    chore: update-libs (#22)
    * chore: update-libs

--- test-e2e/memory.ts
+++ test-e2e/memory.ts
@@ -1,15 +1,17 @@
 import Memory from './memory/index';
 
-// @ts-ignore
 export default {
     paths: ['test-e2e/features/*.feature'],
     require: ['test-e2e/step-definitions/*.ts', 'src/*.ts'],
     format: [
         '@qavajs/console-formatter',
-        '@qavajs/xunit-formatter:test-e2e/report.xml'
+        ['junit', 'test-e2e/report.xml']
     ],
+    formatOptions: {
+        console: {
+            showLogs: true
+        }
+    },
     memory: new Memory(),
-    parallel: 1,
-    retry: 1,
-    publishQuiet: true
+    parallel: 1
 }
